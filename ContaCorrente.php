ca4788ca18d98ff11241223cc2c1a70fc4eb877c
Matheus Bueno <bueno_matheus@outlook.com>
2021-12-29 11:27:19 -0300

    Php orientado a objeto 2

--- ContaCorrente.php
+++ ContaCorrente.php
@@ -23,6 +23,11 @@ class ContaCorrente{
 
     }
 
+    public function __get($atributo){
+        Validacao::protegeAtributo($atributo);
+        return $this->$atributo;
+    }
+
     public function __set($atributo, $valor){
 
         Validacao::protegeAtributo($atributo);
@@ -62,9 +67,7 @@ class ContaCorrente{
 
     }
 
-    public function __get($atributo){
-        return $this->$atributo;
-    }
+
 
   
 /*
